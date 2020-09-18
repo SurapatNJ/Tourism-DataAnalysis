12e6a7bf7dba38a0d5f3ec30e27cb9e9d0f97c32
Th. Paphawarin <56307091+deepnam@users.noreply.github.com>
2020-09-18 14:24:06 +0700

    Update App.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,14 +1,74 @@
-import React from 'react';
+import React , {Component} from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+//import Col from 'react-bootstrap/Col';
 import Tabs from 'react-bootstrap/Tabs'
 import Tab from 'react-bootstrap/Tab'
-import TabPane from 'react-bootstrap/TabPane'
-function App() {
+//import TabPane from 'react-bootstrap/TabPane'
+import { Map, GoogleApiWrapper } from 'google-maps-react';
 
+const mapStyles = {
+  width: '800px',
+  height: '500px'
+};
+
+export class MapContainer extends Component {
+
+  render() {
+    return (
+      <div className="App">
+      <link
+        rel="stylesheet"
+        href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+        integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
+        crossorigin="anonymous"
+      />
+   
+      <header className="App-header">
+
+      </header>
+
+      <section className="App-section">
+      <Container>
+        <Row>
+          <Tabs defaultActiveKey="dashboard" className="tabsetting">
+            <Tab eventKey="dashboard" title="Dashboard"></Tab>
+            <Tab eventKey="poi" title="สถานที่ท่องเที่ยวยอดนิยม"></Tab>
+            <Tab eventKey="trip" title="ทริปยอดนิยม"></Tab>
+          </Tabs>
+        </Row>
+        
+        <Row>
+        <Map
+        google={this.props.google}
+        zoom={14}
+        style={mapStyles}
+        initialCenter={
+          {
+            lat:  13.736717,
+            lng:  100.523186
+          }
+        }
+      />
+      </Row>
+
+      </Container>
+      </section>
+    </div>
+
+    );
+  }
+}
+
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyDf8sqiZUNBWHSQkw3Tqpt5R6LIb4kLdbc'
+})(MapContainer);
+
+
+function App() {
+  
   return (
     <div className="App">
       <link
@@ -17,24 +77,25 @@ function App() {
         integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
         crossorigin="anonymous"
       />
-
+   
       <header className="App-header">
 
       </header>
 
-      <section>
+      <section className="App-section">
       <Container>
         <Row>
-          <Tabs defaultActiveKey="dashboard" id="uncontrolled-tab-example">
-            <Tab eventKey="dashboard" title="dashboard"></Tab>
+          <Tabs defaultActiveKey="dashboard" className="tabsetting">
+            <Tab eventKey="dashboard" title="Dashboard"></Tab>
             <Tab eventKey="poi" title="สถานที่ท่องเที่ยวยอดนิยม"></Tab>
             <Tab eventKey="trip" title="ทริปยอดนิยม"></Tab>
           </Tabs>
         </Row>
+        <MapContainer></MapContainer>
       </Container>
       </section>
     </div>
   );
 }
 
-export default App;
+//export default App;
