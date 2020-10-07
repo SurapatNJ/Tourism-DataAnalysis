c7f8d47e41b88fbb01b1bc913c9148676d94b9d3
Kriangsak Chawalittipakorn <oh0867805093@gmail.com>
2020-10-07 23:40:17 +0700

    test

--- tda/app/views.py
+++ tda/app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+
+
 
 # Create your views here.
+def test(request):
+    gauth = GoogleAuth()
+    gauth.LocalWebserverAuth() # client_secrets.json need to be in the same directory as the script
+    drive = GoogleDrive(gauth)
+    return drive
