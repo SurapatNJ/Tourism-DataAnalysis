e04a35521c996fda9f96a4224d30516ba49fd938
Th. Paphawarin <56307091+deepnam@users.noreply.github.com>
2020-10-28 18:55:31 +0700

    Add files via upload

---
+++ front-end/build/precache-manifest.416267f32a3d0e3db96d9e395e834230.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f018dd593a8e9270c2bbef14f3652f95",
+    "url": "/DA-for-Tourism/index.html"
+  },
+  {
+    "revision": "f389a3dc22cf689341af",
+    "url": "/DA-for-Tourism/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "033e65a1d845e8089677",
+    "url": "/DA-for-Tourism/static/css/main.163fa0b7.chunk.css"
+  },
+  {
+    "revision": "f389a3dc22cf689341af",
+    "url": "/DA-for-Tourism/static/js/2.b6aa03f8.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/DA-for-Tourism/static/js/2.b6aa03f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "033e65a1d845e8089677",
+    "url": "/DA-for-Tourism/static/js/main.a51cf6db.chunk.js"
+  },
+  {
+    "revision": "d447a8a1ffd2c3f2a2ae",
+    "url": "/DA-for-Tourism/static/js/runtime-main.cf8bec23.js"
+  }
+]);
