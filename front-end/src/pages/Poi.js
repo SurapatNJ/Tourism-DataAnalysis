722e20be80f326be278fc51855ac0cf952c854dd
Th. Paphawarin <56307091+deepnam@users.noreply.github.com>
2020-11-12 23:05:00 +0700

    Update Poi.js

--- front-end/src/pages/Poi.js
+++ front-end/src/pages/Poi.js
@@ -1,29 +1,30 @@
-import React , {Component, Fragment, useRef } from 'react';
+import React , {Component} from 'react';
 import '../App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import CardDeck from 'react-bootstrap/CardDeck';
 import InputGroup from 'react-bootstrap/InputGroup';
-import ListGroup from 'react-bootstrap/ListGroup';
-import ProgressBar from 'react-bootstrap/ProgressBar';
 import Jumbotron from 'react-bootstrap/Jumbotron';
-import { Typeahead } from 'react-bootstrap-typeahead';
 import options from './data';
 import 'react-bootstrap-typeahead/css/Typeahead.css';
+import {Typography,Paper,TextField,Select,InputLabel,MenuItem,FormControl,Fab,Card,CardContent,Divider} from "@material-ui/core";
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import RoomIcon from '@material-ui/icons/Room';
+import AccountBalanceIcon from '@material-ui/icons/AccountBalance';
+import LoyaltyIcon from '@material-ui/icons/Loyalty';
+
+//Google Map
+import { Map, GoogleApiWrapper } from 'google-maps-react';
 import { Rectangle } from "google-maps-react";
 
 
 
 const mapStyles = {
-  width: '825px',
+  width: '800px',
   height: '500px',
-  marginTop: '20px',
-  marginLeft: '80px'
+  marginTop: '37px',
+  marginLeft: '95px'
 };
 
 const bounds = {
@@ -34,101 +35,154 @@ const bounds = {
   east: 100.523+0.01
 }
 
-const PlaceName = () => {
-  const ref = useRef();
+export function PlaceName() {
+  const defaultProps = {
+    options: options,
+    getOptionLabel: (option) => option.pname,
+  }
   return (
-    <Fragment>
-      <Typeahead
-        clearButton
+    <div style={{ width: 800 ,height:20,marginTop:-10}}>
+      <Autocomplete
+        {...defaultProps}
         id="placeName"
-        labelKey="pname"
-        options={options}
-        placeholder="ชื่อสถานที่"
-        ref={ref}
-        style={{width:500}}
+        autoComplete
+        includeInputInList
+        renderInput={(params) => <TextField  {...params} label="ชื่อสถานที่ท่องเที่ยว" style={{marginLeft:10}}/>}
       />
-    </Fragment>
+    </div>
   );
-};
-
-class SelectDay extends Component{
-  constructor(){
-      super();
-      this.state = {
-          day:null,
-      }
-      this.handleInputChange = this.handleInputChange.bind(this);
-  }
-  handleInputChange(event) {
-      this.setState({
-          day: event.target.value
-      });
-  }
-  render(){
-      return(
-              <select className="form-control" onChange={this.handleInputChange}>
-                <option selected>Select Day</option>
-                <option value="1">Monday</option>
-                <option value="2">Tuesday</option>
-                <option value="3">Wednesday</option>
-                <option value="4">Thursday</option>
-                <option value="5">Friday</option>
-                <option value="6">Saturday</option>
-                <option value="7">Sunday</option>
-                </select>
-      )  
-  }
 }
 
-class SelectTime extends Component{
-  constructor(){
-      super();
-      this.state = {
-          time:null,
-      }
-      this.handleInputChange = this.handleInputChange.bind(this);
-  }
-  handleInputChange(event) {
-      this.setState({
-          time: event.target.value
-      });
-  }
-  render(){
-      return(
-              <select className="form-control" onChange={this.handleInputChange}>
-                <option selected>Select Time</option>
-                <option value="1">00:00-01:00</option>
-                <option value="2">01:00-02:00</option>
-                <option value="3">02:00-03:00</option>
-                <option value="4">03:00-04:00</option>
-                <option value="5">04:00-05:00</option>
-                <option value="6">05:00-06:00</option>
-                <option value="7">06:00-07:00</option>
-                <option value="8">07:00-08:00</option>
-                <option value="9">08:00-09:00</option>
-                <option value="10">09:00-10:00</option>
-                <option value="11">10:00-11:00</option>
-                <option value="12">11:00-12:00</option>
-                <option value="13">12:00-13:00</option>
-                <option value="14">13:00-14:00</option>
-                <option value="15">14:00-15:00</option>
-                <option value="16">15:00-16:00</option>
-                <option value="17">16:00-17:00</option>
-                <option value="18">17:00-18:00</option>
-                <option value="19">18:00-19:00</option>
-                <option value="20">19:00-20:00</option>
-                <option value="21">20:00-21:00</option>
-                <option value="22">21:00-22:00</option>
-                <option value="23">22:00-23:00</option>
-                <option value="24">23:00-00:00</option>
-                </select>
-      )  
-  }
+export function DateStart() {
+  return (
+    <form noValidate>
+      <TextField
+        id="dateStart"
+        label="วันที่เริ่มต้น"
+        type="date"
+        defaultValue="2020-01-01"
+        InputLabelProps={{
+          shrink: true,
+        }}
+      />
+    </form>
+  );
+}
+export function DateEnd() {
+  return (
+    <form noValidate>
+      <TextField
+        id="dateEnd"
+        label="วันที่สิ้นสุด"
+        type="date"
+        defaultValue="2020-01-01"
+        InputLabelProps={{
+          shrink: true,
+        }}
+      />
+    </form>
+  );
 }
 
 
+export function StartTime() {
+  const [startTime, setStartTime] = React.useState('');
+  const handleChange = (event) => {
+    setStartTime(event.target.value);
+  };
+  return (
+    <div>
+      <FormControl style={{marginLeft:10}}>
+        <InputLabel id="startTime">เวลาเริ่มต้น</InputLabel>
+        <Select
+          labelId="selectStartTime"
+          id="selectStartTime"
+          value={startTime}
+          onChange={handleChange}
+          style={{width:120}}
+        >
+          <MenuItem value={1}>00:00</MenuItem>
+          <MenuItem value={2}>01:00</MenuItem>
+          <MenuItem value={3}>02:00</MenuItem>
+          <MenuItem value={4}>03:00</MenuItem>
+          <MenuItem value={5}>04:00</MenuItem>
+          <MenuItem value={6}>05:00</MenuItem>
+          <MenuItem value={7}>06:00</MenuItem>
+          <MenuItem value={8}>07:00</MenuItem>
+          <MenuItem value={9}>08:00</MenuItem>
+          <MenuItem value={10}>09:00</MenuItem>
+          <MenuItem value={11}>10:00</MenuItem>
+          <MenuItem value={12}>11:00</MenuItem>
+          <MenuItem value={13}>12:00</MenuItem>
+          <MenuItem value={14}>13:00</MenuItem>
+          <MenuItem value={15}>14:00</MenuItem>
+          <MenuItem value={16}>15:00</MenuItem>
+          <MenuItem value={17}>16:00</MenuItem>
+          <MenuItem value={18}>17:00</MenuItem>
+          <MenuItem value={19}>18:00</MenuItem>
+          <MenuItem value={20}>19:00</MenuItem>
+          <MenuItem value={21}>20:00</MenuItem>
+          <MenuItem value={22}>21:00</MenuItem>
+          <MenuItem value={23}>22:00</MenuItem>
+          <MenuItem value={24}>23:00</MenuItem>
+        </Select>
+      </FormControl>
+    </div>
+  );
+}
+
+export function EndTime() {
+  const [endTime, setEndTime] = React.useState('');
+  const handleChange = (event) => {
+    setEndTime(event.target.value);
+  };
+  return (
+    <div>
+      <FormControl>
+        <InputLabel id="endTime">เวลาสิ้นสุด</InputLabel>
+        <Select
+          labelId="selectEndTime"
+          id="selectEndTime"
+          value={endTime}
+          onChange={handleChange}
+          style={{width:120}}
+        >
+          <MenuItem value={1}>00:00</MenuItem>
+          <MenuItem value={2}>01:00</MenuItem>
+          <MenuItem value={3}>02:00</MenuItem>
+          <MenuItem value={4}>03:00</MenuItem>
+          <MenuItem value={5}>04:00</MenuItem>
+          <MenuItem value={6}>05:00</MenuItem>
+          <MenuItem value={7}>06:00</MenuItem>
+          <MenuItem value={8}>07:00</MenuItem>
+          <MenuItem value={9}>08:00</MenuItem>
+          <MenuItem value={10}>09:00</MenuItem>
+          <MenuItem value={11}>10:00</MenuItem>
+          <MenuItem value={12}>11:00</MenuItem>
+          <MenuItem value={13}>12:00</MenuItem>
+          <MenuItem value={14}>13:00</MenuItem>
+          <MenuItem value={15}>14:00</MenuItem>
+          <MenuItem value={16}>15:00</MenuItem>
+          <MenuItem value={17}>16:00</MenuItem>
+          <MenuItem value={18}>17:00</MenuItem>
+          <MenuItem value={19}>18:00</MenuItem>
+          <MenuItem value={20}>19:00</MenuItem>
+          <MenuItem value={21}>20:00</MenuItem>
+          <MenuItem value={22}>21:00</MenuItem>
+          <MenuItem value={23}>22:00</MenuItem>
+          <MenuItem value={24}>23:00</MenuItem>
+        </Select>
+      </FormControl>
+    </div>
+  );
+}
 
 
+
+
+
+
+{/*     ////////       main     ///////    */ }
 export class MapContainer extends Component {
   render() {
     return (
@@ -144,37 +198,50 @@ export class MapContainer extends Component {
       </header>
 
       <section className="App-section">
-      <Container fluid>
-        <Row style={{marginTop:20,marginLeft:150}} noGutters={true}>
+
+      <Container fluid style={{marginTop:60}} noGutters={true}>
+        <Paper elevation={5} style={{width:1200,height:130,marginTop:-15,marginLeft:140}}> 
+        <Row style={{marginTop:20,marginLeft:40}} noGutters={true}>
           <Col md="auto">
-            <InputGroup>
+            <InputGroup style={{marginTop:15}}>
                 <InputGroup.Prepend>
-                  <InputGroup.Text style={{backgroundColor:"#525B76",color:"#FFFFFF"}}>Point of Interest</InputGroup.Text>
+                  <InputGroup.Text style={{backgroundColor:"#284B63",color:"#FFFFFF",borderRadius:5}}>Point of Interest</InputGroup.Text>
                 </InputGroup.Prepend>
               <PlaceName/>
             </InputGroup>
           </Col>
-          <Col md="auto">
-            <InputGroup style={{width:400}}>
-                <InputGroup.Prepend>
-                  <InputGroup.Text style={{backgroundColor:"#525B76",color:"#FFFFFF"}}>Day</InputGroup.Text>
-                </InputGroup.Prepend>
-                <SelectDay/>
-                {/* -------------------------------------- */ }
-                <InputGroup.Prepend>
-                  <InputGroup.Text style={{backgroundColor:"#525B76",color:"#FFFFFF"}}>Time</InputGroup.Text>
-                </InputGroup.Prepend>
-                <SelectTime/>
-            </InputGroup>
-          </Col>
-          <Col>
-                <InputGroup.Append>
-                  <Button variant="secondary">Submit</Button>{' '}
-                </InputGroup.Append>
-          </Col>
-      
         </Row>
+        <Row style={{marginTop:-5,marginLeft:40}}>
+            <InputGroup style={{width:1000,marginTop:15}}>
+                <InputGroup.Prepend>
+                  <InputGroup.Text style={{backgroundColor:"#284B63",color:"#FFFFFF",marginTop:10,marginRight:10,height:40,borderRadius:5}}>Date</InputGroup.Text>
+                </InputGroup.Prepend>
+                  <DateStart/>
+                  <Typography style={{marginLeft:20,marginTop:20,marginRight:20}}>To</Typography>
+                  <DateEnd/>
+                <InputGroup.Prepend>
+                  <InputGroup.Text style={{backgroundColor:"#284B63",color:"#FFFFFF",marginTop:10,marginLeft:50,height:40,borderRadius:5}}>Time</InputGroup.Text>
+                </InputGroup.Prepend>
+                <StartTime/>
+                <Typography style={{marginLeft:20,marginTop:20,marginRight:20}}>To</Typography>
+                <EndTime/>
+            </InputGroup>
+                <InputGroup.Append style={{marginTop:-12}}>
+                  {/*Submit Button */}
+                  <Fab
+                    variant="extended"
+                    size="large"
+                    color="secondary"
+                    aria-label="Add"
+                    style={{width:100,height:60}}
+                   >
+                      Submit
+                  </Fab>
+                </InputGroup.Append>
+        </Row>
+        </Paper>
         <Row>
+          {/*   /////   Map detail //////   */}
           <Col md="auto">
             <Map
             google={this.props.google}
@@ -192,40 +259,63 @@ export class MapContainer extends Component {
               }
             }
             >
-              <Rectangle
-               bounds={bounds}
-               options={{strokeColor: "#FF0000",
-               strokeOpacity: 0.35,
-               strokeWeight: 1,
-               fillColor: "#FF0000",
-               fillOpacity: 0.35,}}
-              />
-            </Map>
 
+            <Paper style={{width:870,height:570,marginLeft:57,marginTop:20}} elevation={5}/>
+            <Fab
+                    variant="extended"
+                    size="large"
+                    color="secondary"
+                    aria-label="Add"
+                    style={{width:800,height:40,marginLeft:95,marginTop:-92}}
+                   >
+                      Open Heat Map
+            </Fab>
+            </Map>
           </Col>
-          <Col style={{marginLeft:780}} md="auto">
-            <Jumbotron style={{backgroundSize:'cover' , backgroundColor:'#525B76', marginTop:20, marginLeft:150 , width:450}}>
-                <CardDeck style={{ width: '450px',marginLeft:-32}}> 
-                  <Card border="primary">
-                    <Card.Header style={{textAlign:"center",fontSize:20}}>รายละเอียดสถานที่ท่องเที่ยว</Card.Header>
-                    <Card.Body>
-                      <ListGroup variant="flush">
-                        <ListGroup.Item>ชื่อสถานที่: </ListGroup.Item>
-                        <ListGroup.Item>ละติจูด-ลองจิจูด: </ListGroup.Item>
-                        <ListGroup.Item>ค่าความนิยม: <ProgressBar variant="warning"animated now={45}/></ListGroup.Item>
-                      </ListGroup>
-                    </Card.Body>
-                    <Card.Footer>
-                      <small className="text-muted"></small>
-                    </Card.Footer>
-                  </Card>
-                </CardDeck>
-                </Jumbotron>
+
+          {/*      /////     card     ///////    */}
+          <Col style={{marginLeft:800}} md="auto">
+            <Jumbotron style={{backgroundSize:'cover', backgroundColor:'#3C6E71', marginTop:20, marginLeft:150 , width:450}}>
+              <Card >
+                <CardContent >
+                  <Typography style={{fontFamily:"csPrajad" ,fontSize:24}}>
+                    สถิติของสถานที่ท่องเที่ยว
+                  </Typography>
+                  <Typography color="textSecondary" style={{fontFamily:"csPrajad",fontSize:16}}>
+                    Tourism Statistics
+                  </Typography>
+                  <br />
+                  <Divider/>
+                  <Typography style={{fontFamily:"csPrajad",fontSize:20}}>
+                    <br />
+                    <AccountBalanceIcon />
+                    ชื่อสถานที่ : 
+                  </Typography>
+                  <br />
+                  <Divider/>
+                 
+                  <Typography style={{fontFamily:"csPrajad",fontSize:20}}>
+                    <br />
+                    <RoomIcon/>
+                    ละติจูด-ลองจิจูด : 
+                  </Typography>
+                  <br />
+                  <Divider/>
+                  <Typography style={{fontFamily:"csPrajad",fontSize:20}}>
+                    <br />
+                    <LoyaltyIcon />
+                    ความนิยม : 
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Jumbotron>
           </Col>
         </Row>
-        <Row style={{marginTop:50}}><Col><Button variant="danger" size="xs" style={{marginLeft:80, width:825}} block>Open Heat Map</Button></Col></Row>
+        <Row><br/><br/><br/><br/><br/></Row>
         </Container>
-        
+
+
+  
       </section>
     </div>
     );
